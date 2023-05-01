67bb401cba5d57ff089107433506e220e1532ac8
Michele Bertolino <mikbert92@gmail.com>
2023-05-01 17:33:48 +0200

    add sessionId Test

--- app/src/test/java/myfirstproject/HelloWorldChromeJupiterTest.java
+++ app/src/test/java/myfirstproject/HelloWorldChromeJupiterTest.java
@@ -4,8 +4,8 @@
 package myfirstproject;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static java.lang.invoke.MethodHandles.lookup;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -13,11 +13,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.SessionId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 class HelloWorldChromeJupiterTest {
 
+    Logger log = LoggerFactory.getLogger(lookup().lookupClass());
     WebDriver driver;
 
     @BeforeAll
@@ -36,9 +41,23 @@ class HelloWorldChromeJupiterTest {
     }
 
     @Test
-    void test() {
-        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+    void testBasicMethods() {
+	String sutUrl = "https://bonigarcia.dev/selenium-webdriver-java/";
+        driver.get(sutUrl);
         assertThat(driver.getTitle()).contains("Selenium WebDriver");
+	assertThat(driver.getCurrentUrl()).isEqualTo(sutUrl);
+	assertThat(driver.getPageSource()).containsIgnoringCase("</html>");
+    }
+
+    @Test
+    void testSessionId(){
+	driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+
+	SessionId sessionId = ((RemoteWebDriver) driver).getSessionId();
+
+	assertThat(sessionId).isNotNull();
+
+	log.debug("The sessionId is {}",sessionId.toString());
     }
 
 }
